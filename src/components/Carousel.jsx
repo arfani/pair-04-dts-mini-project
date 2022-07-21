ebc3bba6ec640ec49df18121875de78b9b3c6e65
arfani <arfanihidayat@gmail.com>
2022-07-21 18:21:44 +0800

    update

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
-import { Grid } from "@mui/material";
 
 import * as fetchMovies from "../services/fetchMovies";
 import "./styles/carousel.css";
