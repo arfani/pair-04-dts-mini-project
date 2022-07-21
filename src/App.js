f46e873249a913977005565ba56aabddf440b76c
arfan <arfanihidayat@gmail.com>
2022-07-21 17:11:17 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import Navbar from "./components/navbar";
-import Home from "./components/home";
-import Footer from "./components/footer";
-import { Routes, Route } from "react-router-dom";
-import FormLogin from "./components/forms";
 import MainPage from "./pages/main";
 function App() {
   return (
